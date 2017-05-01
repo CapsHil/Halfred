19f6116656dc7a66525af175bebfa4689ca9d4ac
Pierre Raby <pierre.raby.pro@gmail.com>
2017-05-02 00:27:05 +0200

    Fix json generating issue

--- server-node/routes.js
+++ server-node/routes.js
@@ -50,7 +50,8 @@ module.exports = function(app) {
         console.log("Get request from " + req.header['x-real-ip'] + " on /api/lastPresence");
         res.status(200);
         presence.getLastPresenceDate(function(date) {
-            res.end(date);
+	    date = date.replace(/(\r\n|\n|\r)/gm,"");
+            res.end('{"date": "'+date+'"}');
         });
     })
 }
